94b5fb21346398f90f4462b2a2ef8cb09598918b
matiasbarg <matiasbarg@hotmail.com>
2022-07-13 02:44:33 -0300

    rompo todo y empiezo de nuevo

--- app.js
+++ app.js
@@ -1,103 +1,133 @@
 
+const numeroPregunta = document.querySelector(".numeroPregunta");
+const textoPregunta = document.querySelector(".textoPregunta");
+const containerOpciones = document.querySelector(".containerOpciones");
+const resultadoRespuesta = document.querySelector(".resultadoRespuesta");
 
-let marcador_general = 0
-let marcador_historia = 0
-let marcador_geografia = 0
-let marcador_cultura = 0
+//ARRAY CON LAS PREGUNTAS - pregunta, opciones y respuesta
 
-
-let cuestionario_historia = [
-    {pregunta:"¿Quién descubrió América?" , respuesta:"Colón" , opciones:"Colón, Magallanes, Washington, Los Vikingos"},
-    {pregunta:"¿En que año se declaró la independencia de Argentina?" , respuesta:"1816" , opciones:"1994, 1492, 2001, 1816"},
-    {pregunta:"¿Quién fue el primer presidente de Argentina?" , respuesta:"Rivadavia" , opciones:"San Martin, Belgrano, Rivadavia,Washington"},
+const cuestionario = [
+    {
+        q:'¿Quien descubrió América?',
+        options:['Colón', 'Magallanes', 'Shakespeare', 'Los Vikingos'],
+        answer:"Colón"
+    },
+    {
+        q:'¿Cuando se declaró la independencia de Argentina?',
+        options:['2001', '1492', '1816', '1994'],
+        answer:"1816"
+    },
+    {
+        q:'¿Quien fue el primer presidente de Argentina?',
+        options:['San Martin', 'Belgrano', 'Rivadavia', 'Washington'],
+        answer:"Rivadavia"
+    },
+    {
+        q:'¿Cual es la montaña mas alta del mundo?',
+        options:['Aconcagua', 'Los Andes', 'Uritorco', 'Everest'],
+        answer:"Everest"
+    },
+    {
+        q:'¿Cuantos decimales tiene el número Pi π?',
+        options:['100', '1000', 'Infinitos', '0'],
+        answer:"Infinitos"
+    },
+    {
+        q:'¿Cuantos continentes hay en el mundo?',
+        options:['6', '7', '8'],
+        answer:"7"
+    },
+    {
+        q:'¿Quién escribió "Romeo y Julieta"?',
+        options:['Cervantes', 'Quevedo', 'Shakespeare'],
+        answer:"Shakespeare"
+    },
 ]
 
-let cuestionario_geografia = [
-    {pregunta:"Cuantos continentes hay en el mundo" , respuesta:7 , opciones:"5 , 6 , 7 , 8"},
-    {pregunta:"¿Cual es la capital de Paraguay?" , respuesta:"Asunción" , opciones:"Buenos Aires, La Paz, Montevideo , Asunción"},
-    {pregunta:"¿Cual es la montaña mas alta del mundo?" , respuesta:"Everest" , opciones:"Aconcagua, Machu Pichu, Everest, Los Andes"},
-]
 
-let cuestionario_cultura = [
-    {pregunta:" ¿Quién es el autor de la frase ``Pienso, luego existo``?" , respuesta:"Descartes" , opciones:"Platon, Galileo Galilei, Descartes, Sócrates"},
-    {pregunta:"¿Cuál es el libro más vendido en el mundo después de la Biblia?" , respuesta:"Don Quijote" , opciones:"El Señor de los Anillos, Don Quijote, El Principitol, Cien años de Soledad"},
-    {pregunta:"¿Cuántos decimales tiene el número pi π?" , respuesta:"infinitos" , opciones:"2 , 100, infinitos, 1000"},
-]
-
-console.log("Vamos a jugar a preguntas y respuestas");
-console.log("Vamos a jugar la categoría Historia");
+let preguntasDisponibles = [];
+let opcionesDisponibles = [];
+let contadorPreguntas = 0;
+let preguntaActual;
+let marcador = 0
 
 
+//PREGUNTAS DISPONIBLES
 
-for(let question of cuestionario_historia){
-    console.log(question.pregunta);
-    console.log(question.opciones);
-    let resp = prompt("Escriba la respuesta:");
-    if(resp == question.respuesta){
-        console.log("Correcto, respondiste " , question.respuesta);
-        marcador_general++;
-        marcador_historia++;
-        console.log("su puntaje es: " , marcador_general);
+function definirPreguntasDisponibles(){
+    const totalPreguntas = cuestionario.length;
+    for(let i=0; i<totalPreguntas; i++){
+        preguntasDisponibles.push(cuestionario[i])
+    }
+}
+
+definirPreguntasDisponibles();
+
+
+//TRAER PREGUNTA
+
+function traerPregunta(){
+    numeroPregunta.innerHTML = "Pregunta " + (contadorPreguntas + 1) + " de " + cuestionario.length; //contador de preguntas
+
+    const indexPreguntas = preguntasDisponibles[Math.floor(Math.random() * preguntasDisponibles.length)]  //random para las preguntas
+
+    preguntaActual = indexPreguntas;
+
+    textoPregunta.innerHTML = preguntaActual.q;   //texto pregunta actual
+    
+    const index1 = preguntasDisponibles.indexOf(indexPreguntas);
+    preguntasDisponibles.splice(index1,1);  //saco la pregunta actual de preguntas disponibles
+
+    const largoOpciones = preguntaActual.options.length; //cantidad de opciones a elegir
+    for(let i=0; i<largoOpciones; i++){
+        opcionesDisponibles.push(i)
+    }
+    containerOpciones.innerHTML = '';
+
+
+    for(let i=0; i<largoOpciones; i++){
+        const optionIndex = opcionesDisponibles[Math.floor(Math.random() * opcionesDisponibles.length)]; //random para las opciones
+        const index2 = opcionesDisponibles.indexOf(optionIndex);
+        opcionesDisponibles.splice(index2,1);
+        
+        const option = document.createElement("div");
+        option.innerHTML = preguntaActual.options[optionIndex];
+        option.className = "option";
+        containerOpciones.appendChild(option)
+    }
+    contadorPreguntas++
+}
+
+traerPregunta();   ///genero pregunta y opciones
+
+function responder(){   //Toma valor del input y compara con la respuesta de la pregunta del array
+    if(respuesta.value == preguntaActual.answer){
+        console.log("correcto!");
+        marcador++
+        resultadoRespuesta.innerHTML = "Correcto!"
     }
     else{
-        console.log("incorrecto");
-        console.log("la respuesta correcta es: " , question.respuesta);
-        console.log("su puntaje es: " , marcador_general);
+        console.log("incorrecto, la respuesta era " , preguntaActual.answer)
+        resultadoRespuesta.innerHTML = "Incorrecto!"
     }
+    siguiente();
 }
 
-console.log("Terminamos con las preguntas de historia, respondiste " , marcador_historia);
-console.log("Vamos a la categoría Geografía");
 
-for(let question of cuestionario_geografia){
-    console.log(question.pregunta);
-    console.log(question.opciones);
-    let resp = prompt("Escriba la respuesta:");
-    if(resp == question.respuesta){
-        console.log("Correcto, respondiste " , question.respuesta);
-        marcador_general++;
-        marcador_geografia++;
-        console.log("su puntaje es: " , marcador_general);
+    function siguiente(){
+        if(contadorPreguntas === cuestionario.length){
+            console.log("quiz finalizado");
+            console.log("Tu puntaje general fue:");
+            console.log(marcador , " puntos en total.");
+
+            if(marcador == cuestionario.length){
+                console.log("Respondiste todas las preguntas correctamente!");
+                resultadoRespuesta.innerHTML = "Puntaje Perfecto!"
+            }
+            else{
+            }
+        }
+        else{
+            traerPregunta();
+        }
     }
-    else{
-        console.log("incorrecto");
-        console.log("la respuesta correcta es: " , question.respuesta);
-        console.log("su puntaje es: " , marcador_general);
-    }
-}
-
-console.log("Terminamos con las preguntas de geografía, respondiste " , marcador_geografia , "correctas.");
-console.log("Vamos a la categoría de Cultura General")
-
-for(let question of cuestionario_cultura){
-    console.log(question.pregunta);
-    console.log(question.opciones);
-    let resp = prompt("Escriba la respuesta:");
-    if(resp == question.respuesta){
-        console.log("Correcto, respondiste " , question.respuesta);
-        marcador_general++;
-        marcador_cultura++;
-        console.log("su puntaje es: " , marcador_general);
-    }
-    else{
-        console.log("incorrecto");
-        console.log("la respuesta correcta es: " , question.respuesta)
-        console.log("su puntaje es: " , marcador_general);
-    }
-}
-
-console.log("Terminamos con las preguntas de Cultura general, respondiste " , marcador_cultura , "correctas.");
-
-
-console.log("Tu puntaje general fue:");
-console.log(marcador_historia , " aciertos en la categoría historia");
-console.log(marcador_geografia , " aciertos en la categoría geografía");
-console.log(marcador_cultura , " aciertos en la categoría Cultura general");
-console.log(marcador_general , " puntos en total.");
-
-if(marcador_general == (cuestionario_historia.length + cuestionario_geografia.length + cuestionario_cultura.length)){
-    console.log("Respondiste todas las preguntas correctamente!");
-}
-else{
-    console.log("te faltaron " , ((cuestionario_historia.length + cuestionario_geografia.length + cuestionario_cultura.length) - marcador_general) , " para el puntaje perfecto")
-}
