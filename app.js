265508f3305a1e8aee19f605b8a4033979e9db8e
matiasbarg <matiasbarg@hotmail.com>
2022-07-16 18:47:54 -0300

    chau inputgit add .!

--- app.js
+++ app.js
@@ -1,103 +1,155 @@
 
-
-let marcador_general = 0
-let marcador_historia = 0
-let marcador_geografia = 0
-let marcador_cultura = 0
-
-
-let cuestionario_historia = [
-    {pregunta:"¿Quién descubrió América?" , respuesta:"Colón" , opciones:"Colón, Magallanes, Washington, Los Vikingos"},
-    {pregunta:"¿En que año se declaró la independencia de Argentina?" , respuesta:"1816" , opciones:"1994, 1492, 2001, 1816"},
-    {pregunta:"¿Quién fue el primer presidente de Argentina?" , respuesta:"Rivadavia" , opciones:"San Martin, Belgrano, Rivadavia,Washington"},
-]
-
-let cuestionario_geografia = [
-    {pregunta:"Cuantos continentes hay en el mundo" , respuesta:7 , opciones:"5 , 6 , 7 , 8"},
-    {pregunta:"¿Cual es la capital de Paraguay?" , respuesta:"Asunción" , opciones:"Buenos Aires, La Paz, Montevideo , Asunción"},
-    {pregunta:"¿Cual es la montaña mas alta del mundo?" , respuesta:"Everest" , opciones:"Aconcagua, Machu Pichu, Everest, Los Andes"},
-]
-
-let cuestionario_cultura = [
-    {pregunta:" ¿Quién es el autor de la frase ``Pienso, luego existo``?" , respuesta:"Descartes" , opciones:"Platon, Galileo Galilei, Descartes, Sócrates"},
-    {pregunta:"¿Cuál es el libro más vendido en el mundo después de la Biblia?" , respuesta:"Don Quijote" , opciones:"El Señor de los Anillos, Don Quijote, El Principitol, Cien años de Soledad"},
-    {pregunta:"¿Cuántos decimales tiene el número pi π?" , respuesta:"infinitos" , opciones:"2 , 100, infinitos, 1000"},
-]
-
-console.log("Vamos a jugar a preguntas y respuestas");
-console.log("Vamos a jugar la categoría Historia");
+const numeroPregunta = document.querySelector(".numeroPregunta");
+const textoPregunta = document.querySelector(".textoPregunta");
+const containerOpciones = document.querySelector(".containerOpciones");
+const resultadoRespuesta = document.querySelector(".resultadoRespuesta");
+//const botonResponder = document.querySelector(".botonResponder")
+//const inputResp = document.getElementById("respuesta");
+const txtanswr = document.getElementsByClassName(".option");
 
 
 
-for(let question of cuestionario_historia){
-    console.log(question.pregunta);
-    console.log(question.opciones);
-    let resp = prompt("Escriba la respuesta:");
-    if(resp == question.respuesta){
-        console.log("Correcto, respondiste " , question.respuesta);
-        marcador_general++;
-        marcador_historia++;
-        console.log("su puntaje es: " , marcador_general);
+let next = document.getElementById("siguiente")
+next.addEventListener("click" , siguiente);
+
+let preguntasDisponibles = [];
+let opcionesDisponibles = [];
+let contadorPreguntas = 0;
+let preguntaActual;
+let marcador = 0;
+let errores = 0;
+
+
+//PREGUNTAS DISPONIBLES
+
+function definirPreguntasDisponibles(){
+    const totalPreguntas = cuestionario.length;
+    for(let i=0; i<totalPreguntas; i++){
+        preguntasDisponibles.push(cuestionario[i])
+    }
+}
+
+
+
+
+//TRAER PREGUNTA
+
+function traerPregunta(){
+    numeroPregunta.innerHTML = "Pregunta " + (contadorPreguntas + 1) + " de " + cuestionario.length; //contador de preguntas
+
+    const indexPreguntas = preguntasDisponibles[Math.floor(Math.random() * preguntasDisponibles.length)]  //random para las preguntas
+
+    preguntaActual = indexPreguntas;
+
+    textoPregunta.innerHTML = preguntaActual.q;   //texto pregunta actual
+    
+    const index1 = preguntasDisponibles.indexOf(indexPreguntas);
+    preguntasDisponibles.splice(index1,1);  //saco la pregunta actual de preguntas disponibles
+
+
+    //mostrar imagen si la propiedad foto existe 
+    if(preguntaActual.hasOwnProperty("foto")){
+        const img = document.createElement("img");
+        img.src = preguntaActual.foto;
+        textoPregunta.appendChild(img);
+    }
+
+    const largoOpciones = preguntaActual.options.length; //cantidad de opciones a elegir
+    for(let i=0; i<largoOpciones; i++){
+        opcionesDisponibles.push(i)
+    }
+    containerOpciones.innerHTML = '';
+
+
+    for(let i=0; i<largoOpciones; i++){
+        const optionIndex = opcionesDisponibles[Math.floor(Math.random() * opcionesDisponibles.length)]; //random para las opciones
+        const index2 = opcionesDisponibles.indexOf(optionIndex);
+        opcionesDisponibles.splice(index2,1);
+        
+        const option = document.createElement("div");
+        option.innerHTML = preguntaActual.options[optionIndex];
+        option.id = optionIndex;
+        option.className = "option";
+        containerOpciones.appendChild(option);
+        option.setAttribute("onclick" , "responder(this)")
+    }
+    contadorPreguntas++
+}
+
+
+
+
+
+
+
+//Toma valor del input y compara con la respuesta de la pregunta del array
+function responder(element){
+    const id = parseInt(element.id);
+    if(id === preguntaActual.answer){
+        marcador++
+        resultadoRespuesta.innerHTML = "Correcto! Aciertos: " + marcador;
+        resultadoRespuesta.classList.add("correcto");
+        resultadoRespuesta.classList.remove("incorrecto");
+        element.classList.add("acierto");
+        //document.getElementsByTagName("input")[0].value = "";
     }
     else{
-        console.log("incorrecto");
-        console.log("la respuesta correcta es: " , question.respuesta);
-        console.log("su puntaje es: " , marcador_general);
+        errores++;
+        resultadoRespuesta.innerHTML = "Incorrecto! Fallos: " + errores;
+        resultadoRespuesta.classList.add("incorrecto");
+        resultadoRespuesta.classList.remove("correcto");
+        const largoOpciones = containerOpciones.children.length;
+        for(i=0; i<largoOpciones; i++){
+            if(parseInt(containerOpciones.children[i].id) === preguntaActual.answer){
+                containerOpciones.children[i].classList.add("acierto");
+            }
+        }
+        element.classList.add("error");
+        //document.getElementsByTagName("input")[0].value = "";
+    }
+    restringido()
+}
+
+function restringido(){
+    const largoOpciones = containerOpciones.children.length;
+    for(let i=0; i<largoOpciones; i++){
+        containerOpciones.children[i].classList.add("respondida")
     }
 }
 
-console.log("Terminamos con las preguntas de historia, respondiste " , marcador_historia);
-console.log("Vamos a la categoría Geografía");
 
-for(let question of cuestionario_geografia){
-    console.log(question.pregunta);
-    console.log(question.opciones);
-    let resp = prompt("Escriba la respuesta:");
-    if(resp == question.respuesta){
-        console.log("Correcto, respondiste " , question.respuesta);
-        marcador_general++;
-        marcador_geografia++;
-        console.log("su puntaje es: " , marcador_general);
+    function siguiente(){
+        if(contadorPreguntas === cuestionario.length){
+            resultadoRespuesta.innerHTML = "Trivia finalizada. Hiciste " + marcador + " puntos en total.";
+            finalizar();
+            if(marcador == cuestionario.length){
+                resultadoRespuesta.innerHTML = "Puntaje Perfecto! Respondiste todas las preguntas correctamente!";
+            }
+            else{
+            }
+        }
+        else{
+
+            traerPregunta();
+        }
     }
-    else{
-        console.log("incorrecto");
-        console.log("la respuesta correcta es: " , question.respuesta);
-        console.log("su puntaje es: " , marcador_general);
+
+    //Eliminar todos los elementos del box salvo el resultado al fnalizar
+
+    function finalizar(){
+        next.remove();
+        textoPregunta.remove();
+        containerOpciones.remove();
+        resultadoRespuesta.classList.remove("correcto" , "incorrecto");
+        resultadoRespuesta.classList.add("margen"); //le doy mas margen arriba y abajo al resultado ya que queda solo en el box
     }
-}
 
-console.log("Terminamos con las preguntas de geografía, respondiste " , marcador_geografia , "correctas.");
-console.log("Vamos a la categoría de Cultura General")
-
-for(let question of cuestionario_cultura){
-    console.log(question.pregunta);
-    console.log(question.opciones);
-    let resp = prompt("Escriba la respuesta:");
-    if(resp == question.respuesta){
-        console.log("Correcto, respondiste " , question.respuesta);
-        marcador_general++;
-        marcador_cultura++;
-        console.log("su puntaje es: " , marcador_general);
+    window.onload = function(){
+        definirPreguntasDisponibles();
+        traerPregunta();
     }
-    else{
-        console.log("incorrecto");
-        console.log("la respuesta correcta es: " , question.respuesta)
-        console.log("su puntaje es: " , marcador_general);
-    }
-}
-
-console.log("Terminamos con las preguntas de Cultura general, respondiste " , marcador_cultura , "correctas.");
-
-
-console.log("Tu puntaje general fue:");
-console.log(marcador_historia , " aciertos en la categoría historia");
-console.log(marcador_geografia , " aciertos en la categoría geografía");
-console.log(marcador_cultura , " aciertos en la categoría Cultura general");
-console.log(marcador_general , " puntos en total.");
-
-if(marcador_general == (cuestionario_historia.length + cuestionario_geografia.length + cuestionario_cultura.length)){
-    console.log("Respondiste todas las preguntas correctamente!");
-}
-else{
-    console.log("te faltaron " , ((cuestionario_historia.length + cuestionario_geografia.length + cuestionario_cultura.length) - marcador_general) , " para el puntaje perfecto")
-}
+/*
+    txtanswr.onclick = function(){
+        console.log(this.txtanswr.value);
+        inputResp.innerHTML = option.value;
+    }*/
---
+++ cuestionario.js
@@ -0,0 +1,59 @@
+//ARRAY CON LAS PREGUNTAS - pregunta, opciones y respuesta
+//agrego algunas preguntas con imagenes
+
+const cuestionario = [
+    {
+        q:'¿Quién es esta persona?',
+        options:['Charly Garcia', 'Spinetta', 'Pity Alvarez', 'Fito Paez'],
+        answer:1,
+        foto: "img/spinetta.jpg"
+    },
+    {
+        q:'¿Quién es esta persona?',
+        options:['Einstein', 'Hawkins', 'Eddison', 'Tesla'],
+        answer:0,
+        foto: "img/einstein.jpg"
+    },
+    {
+        q:'¿Como se llama este cuadro?',
+        options:['La Gioconda', 'La Dama', 'Guernica', 'El Beso'],
+        answer:0,
+        foto: "img/gioconda.jpg"
+    },
+    {
+        q:'¿Quién descubrió América?',
+        options:['Colón', 'Magallanes', 'Shakespeare', 'Los Vikingos'],
+        answer:0,
+    },
+
+    {
+        q:'¿Cuando se declaró la independencia de Argentina?',
+        options:['2001', '1492', '1816', '1994'],
+        answer:2,
+    },
+    {
+        q:'¿Quién fue el primer presidente de Argentina?',
+        options:['San Martin', 'Belgrano', 'Rivadavia', 'Washington'],
+        answer:2,
+    },
+    {
+        q:'¿Cual es la montaña mas alta del mundo?',
+        options:['Aconcagua', 'Los Andes', 'Uritorco', 'Everest'],
+        answer:3,
+    },
+    {
+        q:'¿Cuantos decimales tiene el número Pi π?',
+        options:['100', '1000', 'Infinitos', '0'],
+        answer:2,
+    },
+    {
+        q:'¿Cuantos continentes hay en el mundo?',
+        options:['6', '7', '8'],
+        answer:1,
+    },
+    {
+        q:'¿Quién escribió "Romeo y Julieta"?',
+        options:['Cervantes', 'Quevedo', 'Shakespeare'],
+        answer:2,
+    },
+]
